c8ee5a5520620952db2653e205d006007c977983
kzkr <kazukuro19@gmail.com>
2019-12-27 19:10:51 +0900

    implemented add_rigidbody, add_constrain method

--- baumgarte2d/simulator.py
+++ baumgarte2d/simulator.py
@@ -1,7 +1,28 @@
 import numpy as np
 import sympy
+from typing import Tuple, Union, List
+from .rigidbody import RigidBody
 
 
 class Simulator:
     def __init__(self):
-        pass
+        self.__constrains: List[sympy.Expr] = []
+        self.__bodies: List[RigidBody] = []
+
+    def add_rigidbody(self, rigidbody: RigidBody):
+        if isinstance(rigidbody, RigidBody):
+            self.__bodies.append(rigidbody)
+        else:
+            raise RuntimeError("Unkown type: %s" % type(rigidbody))
+
+    def add_constrain(self, constrain: Union[sympy.Expr, List[sympy.Expr]]):
+        """
+        制約条件を加えるメソッド
+        """
+        if isinstance(constrain, list):
+            for c in constrain:
+                self.add_constrain(c)
+        elif isinstance(constrain, sympy.Expr):
+            self.__constrains.append(constrain)
+        else:
+            raise RuntimeError("Unkown type: %s" % type(constrain))
