06d86ba6fa4c7dd09aed0eae6c73a2b59c36c9da
kzkr <kazukuro19@gmail.com>
2019-12-27 20:11:19 +0900

    implemented calc_cq

--- baumgarte2d/simulator.py
+++ baumgarte2d/simulator.py
@@ -26,3 +26,35 @@ class Simulator:
             self.__constrains.append(constrain)
         else:
             raise RuntimeError("Unkown type: %s" % type(constrain))
+
+    @property
+    def q(self) -> List[sympy.Expr]:
+        """
+        追加された剛体の位置の変数(x, y, theta)のリストを返すメソッド
+        """
+        # 位置の変数
+        return sum([[r.x, r.y, r.theta] for r in self.__bodies], [])
+
+    @property
+    def dot_q(self) -> List[sympy.Expr]:
+        """
+        追加された剛体の位置の変数(dx/dt, dy/dt, d\\theta/dt)のリストを返すメソッド
+        """
+        t = sympy.symbols("t")
+        q = self.q
+        return [sympy.Derivative(x, t) for x in q]
+
+    def calc_cq(self) -> sympy.Matrix:
+        """
+        拘束式 C=0 について，Cを位置変数qで偏微分した行列C_qを返すメソッド
+        C_{q,i,j}=\\frac{\\partial C_i}{\\partial q_j}
+        """
+        # 位置の変数の数
+        n_var = len(self.__bodies) * 3
+        # 制約の数
+        n_constrain = len(self.__constrains)
+
+        # Cq(i,j)の値を計算する関数
+        def f(i, j): return sympy.diff(self.__constrains[i], self.q[j])
+
+        return sympy.Matrix(n_constrain, n_var, f)
