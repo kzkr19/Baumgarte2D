8a5eae680629f41a9a6b76d07687886a2a644cab
kzkr <kazukuro19@gmail.com>
2020-01-04 23:08:03 +0900

    changed double_pendulum.py to draw bodies

--- examples/double_pendulum.py
+++ examples/double_pendulum.py
@@ -2,6 +2,9 @@ import baumgarte2d
 import sympy
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+IMAGE_FILE_PATH: str = "./examples/temp"
 
 
 def main():
@@ -21,6 +24,14 @@ def main():
     # 初期値の設定．順にx0, y0, theta0
     block1.initial_position = np.array([1/np.sqrt(2), 1/np.sqrt(2), np.pi/4])
     block2.initial_position = np.array([3/np.sqrt(2), 3/np.sqrt(2), np.pi/4])
+
+    # 描画の設定
+    block1.width = 2.0
+    block1.height = 0.2
+    block1.color = "red"
+    block2.width = 2.0
+    block2.height = 0.2
+    block2.color = "blue"
 
     # シミュレーションする環境の構築
     simulator = baumgarte2d.Simulator()
@@ -42,6 +53,16 @@ def main():
         dumper=c
     )
 
+    # シミュレーション時間を設定しシミュレーション
+    ts = np.linspace(0, 10, num=500)
+    xs = simulator.simulation(
+        ts, 10, 10, [(l1, 1), (l2, 1), (g, 9.8), (c, 0.05)])
+
+    # 描画
+    if not os.path.exists(IMAGE_FILE_PATH):
+        os.mkdir(IMAGE_FILE_PATH)
+    simulator.draw_all(ts, xs, 2, save_format=IMAGE_FILE_PATH+"/%04d.png")
+
     # 行列を表示
     print(simulator.calc_cq())
     print(simulator.calc_cqdotqq())
@@ -49,26 +70,6 @@ def main():
     print(simulator.calc_ctt())
     print(simulator.calc_force())
     print(simulator.calc_mass())
-
-    # シミュレーション時間を設定しシミュレーション
-    ts = np.linspace(0, 10, num=500)
-    xs = simulator.simulation(
-        ts, 10, 10, [(l1, 1), (l2, 1), (g, 9.8), (c, 0.05)])
-
-    # プロット
-    plt.plot(ts, np.degrees(xs[:, 2]), label="$\\theta_1$")
-    plt.plot(ts, np.degrees(xs[:, 5]), label="$\\theta_2$")
-    plt.xlabel("time")
-    plt.ylabel("$\\theta$")
-    plt.legend()
-    plt.show()
-
-    plt.plot(xs[:, 0], xs[:, 1], label="$(x_1,y_1)$")
-    plt.plot(xs[:, 3], xs[:, 4], label="$(x_2,y_2)$")
-    plt.xlabel("$x$")
-    plt.ylabel("$y$")
-    plt.legend()
-    plt.show()
 
 
 if __name__ == "__main__":
